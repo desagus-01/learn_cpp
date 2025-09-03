b6527acc2c4b5f9d639913c9378f6161c3f28f12
desagus <gugafera2016@outlook.com>
2025-09-03 18:13:27 +0100

    initial code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+
+class Student {
+  std::string m_first = "First";
+  std::string m_last = "Last";
+  int m_id = 0;
+  float m_avg = 0;
+
+public:
+};
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello World!\n";
+  std::vector<float> vec;
+  vec.push_back(42.3);
+  vec.push_back(10.1);
+  vec.push_back(19.5);
+
+  for (auto &a : vec) {
+    std::cout << a << "\n";
+  }
+
   return 0;
 }
